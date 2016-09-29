b09d434c6753f734420d1c043a36eb635f59ef46
Dima Naumchik <ceqadrat@gmail.com>
2016-09-29 12:15:46 +0300

    Made a request with cookie that returns a page lostfilm

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-/**
- * Created by Dima on 28.09.2016.
- */
+const request = require('request');
+
+function getPage(path) {
+    return new Promise((resolve, reject) => {
+        request.get({
+                url: path,
+                headers: {
+                    'Cookie': 'uid=4836786; pass=b7c4f8782513194183fea00106689a04'
+                }
+            },
+            function (err, httpResponse, body) {
+                if (err) reject(err);
+                resolve(body);
+            });
+    });
+}
+
+getPage('http://lostfilm.tv/nrdr2.php?c=191&s=1&e=1')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch(console.error);
